83b0e323f5b164f543afc65bb013c3c54983152e
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-02-06 21:33:09 +0530

    good

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ dotenv.config({path: './config.env'});
 
 export const PORT = 5555;
 export const mongoDBURL =
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.Db_url}`;
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.n7msjwt.mongodb.net/?retryWrites=true&w=majority`;
